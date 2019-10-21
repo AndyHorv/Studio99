b82d250e168aefc37c9c7a0c8c3d0c4e1416cd1e
AndyHorv <a.n.horvath@hotmail.com>
2019-10-21 17:19:30 +0200

    SERIAL BDD

--- src/main/java/studio99/persistence/entity/Film.java
+++ src/main/java/studio99/persistence/entity/Film.java
@@ -3,6 +3,8 @@ package studio99.persistence.entity;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -11,24 +13,27 @@ import javax.persistence.Table;
 public class Film implements Serializable {
 	private Integer id;
 	private String titre;
+	private Integer date;
 	private Integer duree;
 	
 	public Film() {
 		super();
 	}
 
-	public Film(String titre, Integer duree) {
+	public Film(String titre, Integer date, Integer duree) {
 		super();
 		this.titre = titre;
+		this.date = date;
 		this.duree = duree;
 	}
 
 	@Override
 	public String toString() {
-		return "Film [id=" + id + ", titre=" + titre + ", duree=" + duree + "]";
+		return "Film [id=" + id + ", titre=" + titre + ", date=" + date + ", duree=" + duree + "]";
 	}
-	
+
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	public Integer getId() {
 		return id;
 	}
@@ -46,5 +51,12 @@ public class Film implements Serializable {
 	}
 	public void setDuree(Integer duree) {
 		this.duree = duree;
+	}
+	public Integer getDate() {
+		return date;
+	}
+
+	public void setDate(Integer date) {
+		this.date = date;
 	}
 }
